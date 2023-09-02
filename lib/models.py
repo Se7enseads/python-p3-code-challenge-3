c03cefdeb366157506a7f4a8bf48ef3ea78d0e22
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-02 23:07:52 +0300

    Create database schema & fixed table relationships

--- lib/models.py
+++ lib/models.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python3
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey, Column, Integer, String
-from sqlalchemy.orm import relationship, backref
-
 
 engine = create_engine('sqlite:///restaurants.db')
-
 Base = declarative_base()
-
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -18,39 +12,38 @@ class Restaurant(Base):
     name = Column(String(30))
     price = Column(Integer)
 
-    reviews = relationship('Review', backref='restaurant')
+    reviews = relationship('Review', back_populates='restaurant')
 
-    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants', viewonly=True)
 
     def __repr__(self):
-        return f'<Restaurant {self.name}>'
-
+        return f'<Restaurant: {self.id} {self.name} {self.price}>'
 
 class Customer(Base):
     __tablename__= 'customers'
 
     id = Column(Integer(), primary_key=True)
-    item_name = Column(String())
-    value = Column(Integer())
+    first_name = Column(String(10))
+    last_name = Column(String(10))
 
-    reviews = relationship('Review', backref='customer')
+    reviews = relationship('Review', back_populates='customer')
 
-    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers', viewonly=True)
 
     def __repr__(self):
-        return f'<Customer {self.name}>'
-
+         return f'<Customer: {self.id} {self.first_name} {self.last_name}>'
 
 class Review(Base):
     __tablename__ = 'reviews'
 
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(10))
-    last_name = Column(String(10))
+    star_rating = Column(Integer)
 
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    
     customer_id = Column(Integer, ForeignKey('customers.id'))
 
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
+
     def __repr__(self):
-        return f'<Review {self.name}>'
+        return f'<Review: {self.id} {self.star_rating} {self.restaurant_id} {self.customer_id}>'
--- lib/seed.py
+++ lib/seed.py
@@ -7,3 +7,49 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Restaurant, Customer, Review
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///restaurant.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Restaurant).delete()
+    session.query(Customer).delete()
+    session.query(Review).delete()
+
+    fake = Faker()
+
+    restaurants = []
+    for i in range(10):
+        restaurant = Restaurant(
+            name=fake.unique.name(),
+            price=random.randint(1000, 7500),
+        )
+
+        session.add(restaurant)
+        restaurants.append(restaurant)
+
+    customers = []
+    for i in range(10):
+        customer = Customer(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+        )
+
+        session.add(customer)
+        customers.append(customer)
+    
+    reviews = []
+    for restaurant in restaurants:
+        for i in range(random.randint(1,5)):
+            review = Review(
+                star_rating=random.randint(1,10),
+                restaurant_id = random.randint(1,10),
+                customer_id = random.randint(1,10),
+            )
+
+            session.add(review)
+            reviews.append(review)
+
+    session.bulk_save_objects(restaurants, customers, reviews)
+    session.commit()
